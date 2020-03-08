1e005d66f97a8ed6aedd97cef4dd470efb794770
whoknowgyk <770574503@qq.com>
2020-03-08 23:32:33 +0800

    路由封装

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  // key和value一样时也可以简写为router;
+  router:router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './pages/home'
+import Index from './pages/index'
+import Product from './pages/product'
+import Detail from './pages/detail'
+import Cart from './pages/cart'
+import Order from './pages/order'
+import OrderConfirm from './pages/orderConfirm'
+import OrderList from './pages/orderList'
+import OrderPay from './pages/orderPay'
+
+// 加载插件
+Vue.use(Router);
+
+export default new Router({
+    // 配置子路由
+    routes:[
+        {
+            // 一级路由
+            path:'/',
+            name:'home',
+            component:Home,
+            children:[
+                {
+                    // 二级路由
+                    path:'/index',
+                    name:'index',
+                    component:Index,
+                },
+                {
+                    // 动态路由
+                    path:'/product/:id',
+                    name:'product',
+                    component:Product,
+                },
+                {
+                    path:'/detail/:id',
+                    name:'detail',
+                    component:Detail,
+                },
+            ]
+        },
+        {
+            path:'/cart',
+            name:'cart',
+            component:Cart,
+        },
+        {
+            path:'/order',
+            name:'order',
+            component:Order,
+            children:[
+                {
+                    // /order/confirm
+                    path:'/confirm',
+                    name:'order-confirm',
+                    component:OrderConfirm,
+                },
+                {
+                    path:'/list',
+                    name:'order-list',
+                    component:OrderList,
+                },
+                {
+                    path:'/pay',
+                    name:'order-pay',
+                    component:OrderPay,
+                },
+            ]
+        },
+    ]
+})
