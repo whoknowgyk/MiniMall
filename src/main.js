19001519b48f2b01d4eec48c9508946fa89db75e
whoknowgyk <770574503@qq.com>
2020-03-09 16:26:08 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 
+// 根据前端的跨域方式做调整
+// 此处是代理方式
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  // 状态码0代表成功
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    // 10 未登录
+    window.location.href = '/#/login'
+  }else{
+    alert(res.msg);
+  }
+});
+
+// 将axios挂载到vue实例上
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
