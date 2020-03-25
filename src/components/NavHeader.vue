7b336d60fe95104bb78b697328254ebe4851e484
whoknowgyk <770574503@qq.com>
2020-03-25 15:12:01 +0800

    add assets and modify navHeader components

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,108 @@
 <template>
-    <div>nav-header</div>
+    <div class="header">
+        <!-- 顶部菜单栏 -->
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <!-- href="javascript:;" 可以防止页面刷新 -->
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MIUI11</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登陆</a>
+                    <a href="javascript:;">注册</a>
+                    <a href="javascript:;" class="my-cart"><span class="icon-cart"></span>购物车</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo">
+                    <a href="/#/index"></a>
+                </div>
+                <div class="header-menu">
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                        <div class="children">
+                            <ul>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="/imgs/item-box-1.png"/>
+                                        </div>
+                                        <div class="pro-name">小米CC9</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="/imgs/item-box-1.png"/>
+                                        </div>
+                                        <div class="pro-name">小米CC9</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="/imgs/item-box-1.png"/>
+                                        </div>
+                                        <div class="pro-name">小米CC9</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="/imgs/item-box-1.png"/>
+                                        </div>
+                                        <div class="pro-name">小米CC9</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="/imgs/item-box-1.png"/>
+                                        </div>
+                                        <div class="pro-name">小米CC9</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="/imgs/item-box-1.png"/>
+                                        </div>
+                                        <div class="pro-name">小米CC9</div>
+                                        <div class="pro-price">1799元</div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="item-menu">
+                        <span>RedMI</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>小米电视</span>
+                        <div class="children"></div>
+                    </div>
+                </div>
+                <div class="header-search">
+                    <div class="wrapper">
+                        <input type="text" name="keyword">
+                        <!-- href="javascript:; 加入一个空js脚本，使得点击不会跳转 -->
+                        <a href="javascript:;"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,3 +111,168 @@ export default {
     name: 'nav-header',
 }
 </script>
+
+<style lang="scss">
+    @import './../assets/scss/base.scss';
+    @import './../assets/scss/mixin.scss';
+    @import './../assets/scss/config.scss';
+    .header{
+        .nav-topbar{
+            height: 39px;
+            line-height: 39px;
+            background-color: #333333;
+            color:#B0B0B0;;
+            .container{
+                @include flex();
+                a{
+                    display: inline-block;
+                    color: #B0B0B0;
+                    margin-right: 17px;
+                }
+                .my-cart{
+                    width: 110px;
+                    background-color: #FF6600;
+                    text-align: center;
+                    color: #ffffff;
+                    .icon-cart{
+                        /* 引入图片的路径要注意 */
+                        @include bgImg(16px,12px,'/imgs/icon-cart-checked.png');
+                        margin-right: 4px;
+                    }
+                }
+            }
+        }
+        .nav-header{
+            .container{
+                position: relative;
+                height: 112px;
+                @include flex();
+                .header-logo{
+                    display: inline-block;
+                    width: 55px;
+                    height: 55px;
+                    background-color: #FF6600;
+                    a{
+                        display: inline-block;
+                        width: 110px;
+                        height: 55px;
+                        /* 伪类 */
+                        &:before{
+                            /* 占位 非常重要 */
+                            content: ' ';
+                            @include bgImg(55px,55px,'/imgs/mi-logo.png',55px);
+                            transition: margin .2s;
+                        }
+                        &:after{
+                            content: ' ';
+                            @include bgImg(55px,55px,'/imgs/mi-home.png',55px);
+                        }
+                        &:hover:before{
+                            margin-left: -55px;
+                            /* 过渡动画 */
+                            transition: margin .2s;
+                        }
+                    }
+                }
+                .header-menu{
+                    display: inline-block;
+                    padding-left: 209px;
+                    width: 643px;
+                    .item-menu{
+                        display: inline-block;
+                        color: #333333;
+                        font-weight: bold;
+                        font-size: 16px;
+                        line-height: 112px;
+                        margin-right: 20px;
+                        span{
+                            cursor: pointer;
+                        }
+                        .children{
+                            position: absolute;
+                            top: 112px;
+                            left: 0;
+                            width: 1226px;
+                            height: 0;
+                            opacity: 0;
+                            border-top: 1px solid #ee5555;
+                            box-shadow: 0 7px 6px 0 rgba(0, 0, 0, 0.11);
+                            z-index: 10;
+                            overflow: hidden;
+                            transition: height .2s;
+                            .product{
+                                position: relative;
+                                float: left;
+                                width: 16.6%;
+                                height: 220px;
+                                font-size: 12px;
+                                line-height: 12px;
+                                text-align: center;
+                                a{
+                                    display: inline-block;
+                                }
+                                img{
+                                    height: 110px;
+                                    width: auto;
+                                    margin-top: 26px;
+                                }
+                                .pro-img{
+                                    height: 137px;
+                                }
+                                .pro-name{
+                                    font-weight: bold;
+                                    margin-top: 19px;
+                                    margin-bottom: 8px;
+                                    color: #333333;
+                                }
+                                .pro-price{
+                                    color: #FF6600;
+                                }
+                                &:before{
+                                    content: ' ';
+                                    position: absolute;
+                                    top: 28px;
+                                    right: 0;
+                                    border-left: 1px solid #d7d7d7;
+                                    height: 100px;
+                                    width: 1px;
+                                }
+                                &:last-child::before{
+                                    display: none;
+                                }
+                            }
+                        }
+                        &:hover{
+                            color: $colorA;
+                            .children{
+                                height: 220px;
+                                opacity: 1;
+                            }
+                        }
+                    }
+                }
+                .header-search{
+                    width: 319px;
+                    .wrapper{
+                        height: 50px;
+                        border: 1px solid #e0e0e0;
+                        display: flex;
+                        align-items: center;
+                        input{
+                            border: none;
+                            box-sizing: border-box;
+                            border-right: 1px solid #e0e0e0;
+                            width: 264px;
+                            height: 50px;
+                            padding-left: 15px;
+                        }
+                        a{
+                            @include bgImg(18px,18px,'/imgs/icon-search.png');
+                            margin-left: 17px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
